e85147a4d0d9694058f70b7decc297b2fb93e47a
Rishav Karanjit <karanjitrishav4@gmail.com>
2020-11-07 18:14:48 +0545

    Updated lsa.c

--- A1. Link State Routing/lsa.c
+++ A1. Link State Routing/lsa.c
@@ -1,117 +1,114 @@
-#include<stdio.h>
-#include<stdlib.h>
-#define inf 999
-int spath(int,int);
-int cost[10][10],n;
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-//for directed graph - fixed input
+#define INFINITY 999
+#define MAX 100
 
-void main()
+int cost[MAX][MAX]; // cost matrix
+int distance[MAX]; // distance from source
+int visited[MAX] = {0};
+int parent[MAX];
+int source;
+int n; // number of nodes
+
+void initialize()
 {
-  int i,j,min,s,d;
-  printf("\n the number of nodes = 5 \n");
-  //scanf("%d",&n);
-  n=5;
-  //printf("enter the cost");
+  int i;
+  visited[source] = 1;
+  parent[source] = source;
 
-
-  /*for(i=1;i<=n;i++)
-  for(j=1;j<=n;j++)
+  for(i=0; i<n; i++)
   {
-   scanf("%d",&cost[i][j]);
-   if(cost[i][j]==0 && i!=j)
-   {
-   cost[i][j]=inf;
-   }
-
-  }  */
-  for(i=1;i<=n;i++)
-   {
-    for(j=1;j<=n;j++) 
+    distance[i] = cost[source][i];
+    if( cost[source][i] != INFINITY )
     {
-         cost[i][j]=999;     // initially no connection
-         if(i==j)            //same node - diagonal in cost matrix
-              cost[i][j]=0 ;
+      parent[i] = source;
     }
-  }
-  cost[1][2]=10;   // direction based
-  cost[1][4]=5;
-  cost[2][3]=6;
-  cost[2][5]=10;
-  cost[3][4]=3;
-  cost[3][5]=6;
-  cost[4][5]=8; 
-  printf("\nsending hello packets \n");
-  for(i=1;i<=n;i++)
-    for(j=1;j<=n;j++)
-    {
-    if(cost[i][j]!=0 && cost[i][j]!=inf)
-      printf("\n sending hello packets from %d to %d node",i,j);
-    }
-  //sleep(2);
-  printf("\nsending echo packets");
-  for(i=1;i<=n;i++)
-    for(j=1;j<=n;j++)
-    {
-      if(cost[i][j]!=0 && cost[i][j]!=inf)
-      printf("sending echo packets from %d to %d node \n",i,j);
-    }
-  //sleep(2);
-  printf("constructing link state protocol");
-  for(i=1;i<=n;i++)
-  {
-    printf("\n**** from  node = %d****\n",i);
-    for(j=1;j<=n;j++)
-    {
-      if(cost[i][j]!=0 && cost[i][j]!=inf)
-      printf("to node = %d--> cost = %d\n",j,cost[i][j]);
-    }
-  }
-  printf(" the source = 1 \n");
-  s=1;
-  //scanf("%d",&s);
-  for(i=1;i<=n;i++)
-  {
-    min=spath(s,i);
-    printf("min path from %d to %d =%d\n",s,i,min);
   }
 }
 
-int spath(int s,int d)
+/* Get Minimum Node Not In Network */
+int GetMin()
 {
-  struct path{
-    int len;
-    enum{tentative,confirmed}label;
-  }state[10];
+  int minIdx = -1;
+  int minDist = INFINITY;
 
-  int i,u,j,num=2,min=inf;
-  
-  for(i=1;i<=n;i++)
+  int i;
+  for(i=0; i<n; i++)
   {
-    state[i].label=tentative;
-    state[i].len=cost[s][i];
-  }
-  state[s].label=confirmed;
-  while(num<=n)
-  {
-    min=inf;
-    for(i=1;i<=n;i++)
+    if( !visited[i] && minDist >= distance[i] )
     {
-      if(state[i].label==tentative && state[i].len<min)
-      {
-        min=state[i].len;
-        u=i;
-      }
-    }
-    state[u].label=confirmed;
-    num++;
-    for(j=1;j<=n;j++)
-    {
-      if(state[u].len + cost[u][j]<state[j].len && state[j].label==tentative)
-      {
-        state[j].len=state[u].len+cost[u][j];
-      }
+      minIdx = i;
+      minDist = distance[i];
     }
   }
-  return state[d].len;
+  return minIdx;
 }
+
+/* update distance for adjacent nodes */
+void updateTable(int node)
+{
+  int i;
+  for(i=0; i<n; i++)
+  {
+    if( cost[node][i] != INFINITY && distance[i] > distance[node]+cost[node][i] )
+    {
+      distance[i] = distance[node] + cost[node][i];
+      parent[i] = node;
+    }
+  }
+}
+
+void display()
+{
+  int i;
+  int node;
+
+  printf("\nNode \t Distance from source \t Path \n");
+  for(i=0; i<n; i++)
+  {
+    printf("%d \t\t %d \t\t", i, distance[i]);
+
+        // node <- parent[node] <- parent[parent[node]] <- ... <- source
+    node = i;
+    printf("%d", node);
+    while( node != source)
+    {
+      printf(" <- %d", parent[node]);
+      node = parent[node];
+    }
+    printf("\n");
+  }
+}
+
+int main()
+{
+  int i, j, node;
+
+  printf("Enter the number of nodes: ");
+  scanf("%d", &n);
+
+  printf("Enter the source node    : ");
+  scanf("%d", &source);
+
+  printf("\nEnter the cost matrix: \n");
+  for(i=0; i<n; i++)
+  {
+    for(j=0; j<n; j++)
+    {
+      scanf("%d", &cost[i][j]);
+    }
+  }
+
+  initialize();
+
+    for(i=0; i<n-1; i++) // for all remaining vertices(since source is already visited)
+    {
+      node = GetMin();
+      visited[node] = 1;
+      updateTable(node);
+    }
+    display();
+    return 0;
+  }
